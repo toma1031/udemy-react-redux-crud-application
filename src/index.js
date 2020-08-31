0de63e66b1d690ff358ab83b632aeba552266f8b
Akinori Toma <akinoritoma@appletoma.home>
2020-08-31 04:56:35 -0400

    implement component with connect function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redex'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import './index.css';
 import reducer from './reducers'
-import App from './component/App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducer)
 
-ReactDOM.render
+ReactDOM.render(
  <Provider store={store}>
-  (<App />
+  <App />
  </Provider>, 
  document.getElementById('root')
 );
--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,8 +1,8 @@
-import { INCREMNT, DECREMENT } from '../actions'
+import { INCREMENT, DECREMENT } from '../actions'
 
 const initialState = { value: 0 }
 
-export default (state = initialState, action) -> {
+export default (state = initialState, action) => {
  switch (action.type) {
   case INCREMENT:
    return { value:state.value +1 }
