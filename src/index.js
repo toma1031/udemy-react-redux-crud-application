6134ad3a58f513a687fd00db4a1ddb85ca19524b
Akinori Toma <akinoritoma@appletoma.home>
2020-09-02 05:57:39 -0400

    implement events index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from'redux-thunk'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './index.css';
 import reducer from './reducers'
-import App from './components/App';
+import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer)
+const store = createStore(reducer,applyMiddleware(thunk))	
 
 ReactDOM.render(
  <Provider store={store}>
-  <App />
+  <BrowserRouter>
+   <Switch>
+    <Route exact path="/events/new" component={EventsNew} />
+    <Route exact path="/" component={EventsIndex} />
+   </Switch>
+  </BrowserRouter>
  </Provider>, 
  document.getElementById('root')
 );
