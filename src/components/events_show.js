93ef565b3a8d1fb77191784a3955c0873219fec9
Akinori Toma <akinoritoma@appletoma.home>
2020-09-07 05:54:50 -0400

    apply material-ui to events_new and events_show components

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux'
 import { Field,reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextFieled from 'material-ui/TextField'
+
 
 import { getEvent, deleteEvent, putEvent } from '../actions'
 
@@ -19,12 +22,17 @@ class EventsShow extends Component {
 
  renderField(field) {
   const { input, label, type, meta: { touched, error } } = field
+  const style = { margin: 12 }
 
   return(
-   <div>
-    <input {...input} placeholder={label} type={type} />
-    {touched && error && <span>{error}</span>}
-   </div>
+   <TextFieled
+    hintText={label}
+    floatingLabelText={label}
+    type={type}
+    errorText={touched && error }
+    { ...input}
+    fullWidth={true}
+   />
   )
  } 
 
@@ -43,6 +51,7 @@ class EventsShow extends Component {
 
  render(){
   const { handleSubmit, pristine, submitting, invalid } = this.props
+  const style = { margin: 12 }
 
   return(
    <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -50,8 +59,9 @@ class EventsShow extends Component {
     <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
 
     <div>
-     <input type="submit" value="Submit" disabled={pristine || submitting || invalid } />
-     <Link to="/" >Cancel</Link>
+     <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid} />
+     <RaisedButton label="Cancel" style={style} containerElement={<Link to="/" />}/>
+     <RaisedButton label="Delete" style={style} onClick={this.onDeleteClick}/>
      <Link to="/" onClick={this.onDeleteClick} >Delete</Link>
     </div>
    </form>
